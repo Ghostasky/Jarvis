ecc39ac7f20f2233d095267655a681e472ae74b7
Ghostasky <2992721672@qq.com>
2023-04-21 15:58:22 +0800

    update and fix some function

--- Jarvis/jarvis.py
+++ Jarvis/jarvis.py
@@ -5,11 +5,11 @@ import openai
 import datetime
 import os
 import sys
-# from getKey import getKeyFromConfig
 from Jarvis.getKey import getKeyFromConfig
 from distutils.sysconfig import get_python_lib
 import getpass
-
+# import Jarvis.role
+import role
 def sendMessage(messageLog):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -19,7 +19,7 @@ def sendMessage(messageLog):
         temperature=0.7,        
     )
     return response.choices[0].message.content
-    pass
+
 def getInput():
     print("")
     line =input("\033[32m>\033[0m ")
@@ -31,52 +31,85 @@ def getInput():
             break
         # 下面用来写其他命令，之后再说
         if '/' == line[0]:
-            return line
+            aaaa = line[0].split()
+            return aaaa
 
         lines.append(a)
     print("[*]Has been sent.\n")
     return '\n'.join(lines)
-    pass
 
 
 
 
 openai.api_key = getKeyFromConfig()
-messageContent = "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Knowledge cutoff: "+str(datetime.datetime.now())[0:10]+" Current date: "+str(datetime.datetime.now())[0:10]
-messages = [{"role": "system", "content": messageContent}]
-
+# messages = [{"role": "system", "content": Jarvis.role.role_1}]
+messages = [{"role": "system", "content": role.role_1}]
 filePath= r"C:/Users/" + getpass.getuser()+"/Jarvis_log/"
 
 
-def main():
+def showInterface():
     Art=art.text2art("jarvis","rand")
     print(Art)
     print("Current Date: "+str(datetime.datetime.now())[0:10]+"."+"power by Ghostasky".rjust(100-len("Current Date: "+str(datetime.datetime.now())[0:10]+".")))
     print("="*100)
+    pass
+
+
+def mainLoop():
     try:
         while True:
             user_input = getInput()
+            
             fileName = str(datetime.datetime.now())[0:10]+'.txt'
             pathAndFile = filePath+fileName
-            # print(pathAndFile)
+            writeLogInfo(pathAndFile,user_input,"Q")
 
-            with open(pathAndFile,"a+",encoding="utf-8") as file:
-                now_time = str(datetime.datetime.now())
-                file.write(str(now_time)[:-7]+"| Q: "+user_input+"\n")
-                pass
             messages.append({"role": "user", "content": user_input})
             response = sendMessage(messages)
-            with open(pathAndFile,"a+",encoding="utf-8") as file:
-                now_time = str(datetime.datetime.now())
-                file.writelines(str(now_time)[:-7]+"| A: "+response+"\n")
-                pass    
+
+            writeLogInfo(pathAndFile,response,"A")
+
             print(f"\033[34mJarvis>\033[0m {response}\n")
-            # \033[31mException: {}\033[0m"
+
     except (KeyboardInterrupt, EOFError) as e:
         print("\nBye Bye~")
         os._exit(0)
 
+def writeLogInfo(filePath,Input, QorA):
+    if QorA == "Q":
+        with open(filePath,"a+",encoding="utf-8") as file:
+            now_time = str(datetime.datetime.now())
+            file.write(str(now_time)[:-7]+"| Q: "+Input+"\n")
+            file.close()
+        pass
+    elif QorA  == "A":
+        with open(filePath,"a+",encoding="utf-8") as file:
+            now_time = str(datetime.datetime.now())
+            file.writelines(str(now_time)[:-7]+"| A: "+Input+"\n")
+            file.close()
+        pass
 
+
+def showHelpInfo():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l',"--list_all_role",help="list all role",action="store_true")
+    args = parser.parse_args()
+    if args.list_all_role:
+        print("1. You are ChatGPT, a large language model....")
+        print("2. bypass openai ")
+
+def changerole():
+    pass
+def test(a):
+    if a[0]=='/':
+        print("change role")
+    pass
+def main():
+    a = getInput()
+    test(a)
+    # showInterface()
+    # showHelpInfo()
+    # mainLoop()
 
 if __name__ == '__main__':
     main()
